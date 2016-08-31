60cd710da257452960ab82821979310bf71e320b
Minh Nguyen <mnguyen284@gmail.com>
2016-09-01 02:07:20 +0700

    1.0.0-rc1-1

---
+++ dist/js/directives/pn-utilities.js
@@ -0,0 +1,26 @@
+(function () {
+
+    'use strict';
+
+    var module = angular.module('pnUtilities', []);
+
+    module.directive('pnButtonsPagination', function () {
+        return {
+            restrict: 'EA',
+            replace: true,
+            scope: {
+                pageCount: '&',
+                pageNumber: '=',
+                maxPageLength: '&',
+                pageLabelFormat: '&'
+            },
+            templateUrl: '/assets/_vendors/pn-ng-utilities/dist/templates/pn-buttons-pagination.html',
+            link: function (scope) {
+                scope.goToPage = function (pageNumber) {
+                    scope.pageNumber = pageNumber;
+                };
+            }
+        };
+    });
+
+})();
