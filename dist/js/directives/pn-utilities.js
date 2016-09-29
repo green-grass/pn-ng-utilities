f9d7a4d46f0a27cfca922e3343f284d3d36f7539
Minh Nguyen <mnguyen284@gmail.com>
2016-09-29 15:37:45 +0700

    1.0.0-rc1-4

--- dist/js/directives/pn-utilities.js
+++ dist/js/directives/pn-utilities.js
@@ -4,6 +4,18 @@
 
     var module = angular.module('pnUtilities');
 
+    module.directive('pnDropdownPagination', function () {
+        return {
+            restrict: 'EA',
+            replace: true,
+            scope: {
+                pageCount: '&',
+                currentPage: '='
+            },
+            templateUrl: '/assets/_vendors/pn-ng-utilities/dist/templates/pn-dropdown-pagination.html'
+        };
+    });
+
     module.directive('pnButtonsPagination', function () {
         return {
             restrict: 'EA',
@@ -22,5 +34,22 @@
             }
         };
     });
+
+    module.directive('pnCombinedPagination', ['$animate', function ($animate) {
+        return {
+            restrict: 'EA',
+            replace: true,
+            scope: {
+                pageCount: '&',
+                currentPage: '=',
+                maxPageLength: '&',
+                pageLabelFormat: '&'
+            },
+            templateUrl: '/assets/_vendors/pn-ng-dashboard/dist/templates/pn-combined-pagination.html',
+            link: function (scope, element) {
+                scope.showPageJumper = false;
+            }
+        };
+    }]);
 
 })();
